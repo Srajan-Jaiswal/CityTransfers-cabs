8b624c6bedaa7c528d7b556a363411468d287455
Srajan-Jaiswal <jaiswalsrajan2026@gmail.com>
2020-12-25 22:38:39 +0530

    Database Connected

---
+++ lib/mainpage.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Main Page")
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: (){
+             DatabaseReference dbref = FirebaseDatabase.instance.reference().child('Test1');
+             dbref.set('IsConnected');
+          },
+          height: 50,
+          minWidth: 300,
+          color: Colors.green,
+          child: Text('Testing Connection'),
+        ),
+      )
+      
+    );
+  }
+}
